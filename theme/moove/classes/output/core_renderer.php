54a55cd123d4a30b84f3503da4331db94e450d26
Mona <mona.shakibapour@wunderbyte.at>
2025-10-29 14:34:48 +0100

    Update Moove theme: adjusted core_renderer and general SCSS

--- theme/moove/classes/output/core_renderer.php
+++ theme/moove/classes/output/core_renderer.php
@@ -70,11 +70,7 @@ class core_renderer extends \theme_boost\output\core_renderer {
         $sitefont = isset($theme->settings->fontsite) ? $theme->settings->fontsite : 'Moodle';
 
         if ($sitefont != 'Moodle') {
-            $output .= '<link rel="preconnect" href="https://fonts.googleapis.com">
-                       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                       <link href="https://fonts.googleapis.com/css2?family='
-                . $sitefont .
-                ':ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">';
+            $output .= '';
         }
 
         return $output;
